fa8885c5c666ddb815d7569d5974fe2b4d6f5a58
ravanmaxmud <ravanim@code.edu.az>
2022-07-01 20:53:11 +0400

    Methods added

--- PersonManagement/PersonManagement/Program.cs
+++ PersonManagement/PersonManagement/Program.cs
@@ -5,20 +5,20 @@ namespace PersonManagement
 {
     internal class Program
     {
+           public static List<Person> persons = new List<Person>();
         static void Main(string[] args)
         {
-            List<Person> persons = new List<Person>();
 
             Console.WriteLine("Our available commands :");
             Console.WriteLine("/add-new-person");
             Console.WriteLine("/remove-person");
             Console.WriteLine("/show-persons");
+            Console.WriteLine("/remove-all-persons");
             Console.WriteLine("/exit");
 
             while (true)
             {
                 Console.WriteLine();
-
                 Console.Write("Enter command : ");
                 string command = Console.ReadLine();
 
@@ -32,35 +32,23 @@ namespace PersonManagement
 
                     Console.Write("Please add person's FIN code :");
                     string fin = Console.ReadLine();
-
-                    Person person = new Person(name, lastName, fin);
-                    persons.Add(person);
-
-                    Console.WriteLine(person.GetInfo() + " - Added to system.");
-
+                    AddNewPersons(name, lastName, fin);
                 }
                 else if (command == "/remove-person")
                 {
                     Console.Write("To remove person, please enter his/her FIN code : ");
                     string fin = Console.ReadLine();
-                    for (int i = 0; i < persons.Count; i++)
-                    {
-                        if (persons[i].FIN == fin)
-                        {
-                            Console.WriteLine(persons[i].GetInfo());
-                            persons.RemoveAt(i);
-                            Console.WriteLine("Person removed successfully");
-                        }
-                    }
-
+                    RemovePerson(fin);
                 }
                 else if (command == "/show-persons")
                 {
                     Console.WriteLine("Persons in database : ");
-                    foreach (Person person in persons)
-                    {
-                        Console.WriteLine(person.GetInfo());
-                    }
+                    ShowPersons();
+                }
+                else if (command == "/remove-all-persons")
+                {
+                    RemoveAllPersons();
+                    Console.WriteLine("All Persons Removed Succesifully");
                 }
                 else if (command == "/exit")
                 {
@@ -72,6 +60,40 @@ namespace PersonManagement
                     Console.WriteLine("Command not found, please enter command from list!");
                     Console.WriteLine();
                 }
+            }
+        }
+
+        public static void AddNewPersons(string name,string lastName,string fin)
+        {
+            Person person = new Person(name, lastName, fin);
+            persons.Add(person);
+
+            Console.WriteLine(person.GetInfo() + " - Added to system.");
+        }
+        public static void RemovePerson(string fin)
+        {
+            for (int i = 0; i < persons.Count; i++)
+            {
+                if (persons[i].FIN == fin)
+                {
+                    Console.WriteLine(persons[i].GetInfo());
+                    persons.RemoveAt(i);
+                    Console.WriteLine("Person removed successfully");
+                }
+            }
+        }
+        public static void ShowPersons()
+        {
+            foreach (Person person in persons)
+            {
+                Console.WriteLine(person.GetInfo());
+            }
+        }
+        public static void RemoveAllPersons()
+        {
+            for (int i = 0; i <= persons.Count; i++)
+            {
+                persons.RemoveAt(0);
             }
         }
     }
